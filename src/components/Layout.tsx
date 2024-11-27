d106084f8ac952354c98c89cc9de27fd160061cc
27xPriyanshu <priyanshuambavane5@gmail.com>
2024-11-27 14:57:26 +0530

    EDIT

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import { BsShop } from "react-icons/bs";
 import CartDrawer from "./CartDrawer";
 import Signup from "./Signup";
+import { isLoggedIn, signupformOpen } from "@/utils/store";
 
+<<<<<<< HEAD
 const Layout: React.FC = () => {
   const [isCartVisible, setIsCartVisible] = useState(false);
 
@@ -67,6 +69,51 @@ const Layout: React.FC = () => {
     </>
   );
 };
+=======
+function Layout() {
+	const [iscartVisible, setIscartVisible] = useState(false);
+
+	const singupformState = useContext(signupformOpen);
+
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const { loggedIn } = isLoggedIn();
+	console.log("loggedin", loggedIn);
+
+	return (
+		<signupformOpen.Provider value={{ isOpen, setIsOpen }}>
+			<header className='w-screen max-w-7xl    h-fit relative flexCenter'>
+				<Link
+					to={"/"}
+					className='font-bold text-blue-400 font-serif mr-auto text-2xl'>
+					ApnaShop
+				</Link>
+				<Link to={"/product-archive"}>Shop</Link>
+				{!loggedIn && (
+					<button
+						onClick={() => {
+							setIsOpen((prev: boolean) => !prev);
+						}}>
+						Login
+					</button>
+				)}
+				<button
+					onClick={() => {
+						setIscartVisible((prev) => !prev);
+					}}>
+					Cart
+				</button>
+			</header>
+			<Outlet />
+			<CartDrawer
+				setIscartVisible={setIscartVisible}
+				isVisible={iscartVisible}
+			/>
+			<footer>This Is my Footer</footer>
+			<Signup />
+		</signupformOpen.Provider>
+	);
+}
+>>>>>>> 3c0d68492cc23ab72174118ebfaee39627fd0194
 
 export default Layout;
 
